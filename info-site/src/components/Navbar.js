cdbbcb1183a2a0240a4be9370166ab2c17e19275
priyanka <priyankaraju92@gmail.com>
2022-12-22 10:41:41 -0500

    scrimba

---
+++ info-site/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            {/*<img src="../images/react-icon-small.png" className="nav--icon" />*/}
+            <img src="/Users/priyankaraju/repos/react-infosite/info-site/public/logo512.png" className="nav--icon" alt="pic" />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    )
+}
