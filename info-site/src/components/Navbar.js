be781c416c9f0538a2f3eadcb52d1c603108ea58
priyanka <priyankaraju92@gmail.com>
2022-12-30 22:03:10 -0500

    pic problem

--- info-site/src/components/Navbar.js
+++ info-site/src/components/Navbar.js
@@ -4,7 +4,7 @@ export default function Navbar() {
     return (
         <nav>
             {/*<img src="../images/react-icon-small.png" className="nav--icon" />*/}
-            <img src="/Users/priyankaraju/repos/react-infosite/info-site/public/logo512.png" className="nav--icon" alt="pic" />
+            <img src={require("../images/logo512.png")} className="nav--icon" alt="pic" />
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
         </nav>
